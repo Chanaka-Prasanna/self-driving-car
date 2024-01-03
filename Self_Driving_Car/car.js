85666dc79776a43ff10ee62ad7eb62912983390b
Chanaka <chanaka7518@gmail.com>
2024-01-03 20:31:13 +0530

    Completed the car movements

--- Self_Driving_Car/car.js
+++ Self_Driving_Car/car.js
@@ -15,6 +15,10 @@ class Car {
   }
 
   update() {
+    this.#move();
+  }
+
+  #move() {
     if (this.controls.forward) {
       this.speed += this.acceleration;
     }
@@ -42,27 +46,38 @@ class Car {
       this.speed = 0;
     }
 
-    // left and right direction
+    // left and right direction and manage reverse of the car
 
-    if (this.controls.left) {
-      this.angle += 0.03;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
 
-    if (this.controls.right) {
-      this.angle += 0.03;
-    }
-
-    this.y -= this.speed;
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+    // this.y -= this.speed;
   }
   draw(ctx) {
+    ctx.save(); // save the context
+    ctx.translate(this.x, this.y); // tralslate car to x and y
+    ctx.rotate(-this.angle); // rotate
     ctx.beginPath();
     // rect -> rectangle to drw a car
-    ctx.rect(
-      this.x - this.width / 2, // X-coordinate of the top-left corner of the rectangle
-      this.y - this.height / 2, // Y-coordinate of the top-left corner of the rectangle
+    ctx.fillStyle = "#0e7678";
+    ctx.fillRect(
+      -this.width / 2, // X-coordinate of the top-left corner of the rectangle
+      -this.height / 2, // Y-coordinate of the top-left corner of the rectangle
       this.width,
       this.height
     );
     ctx.fill();
+
+    ctx.restore(); // to remove the infinit rotation and translation
   }
 }
